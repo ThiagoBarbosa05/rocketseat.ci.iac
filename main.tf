ec1036eca30706893d25b59ed0a73aade7eece8a
ThiagoBarbosa05 <thiagon16@hotmail.com>
2024-11-06 15:18:34 -0300

    new: first commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.69.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+  profile = "AdministratorAccess-686255959518"
+}
+
+#  aws s3 ls --profile AdministratorAccess-686255959518
