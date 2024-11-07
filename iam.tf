1862b5b681eb829d6b20bc79bcfce7331e11e20d
ThiagoBarbosa05 <thiagon16@hotmail.com>
2024-11-07 14:32:22 -0300

    fix: ecr role

--- iam.tf
+++ iam.tf
@@ -42,99 +42,99 @@ resource "aws_iam_role" "tf-role" {
   }
 }
 
-resource "aws_iam_role" "app-runner-role" {
-  name = "app-runner-role"
+# resource "aws_iam_role" "app-runner-role" {
+#   name = "app-runner-role"
 
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "build.apprunner.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
-    ]
-  })
+#   assume_role_policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Effect" : "Allow",
+#         "Principal" : {
+#           "Service" : "build.apprunner.amazonaws.com"
+#         },
+#         "Action" : "sts:AssumeRole"
+#       }
+#     ]
+#   })
 
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  ]
+#   managed_policy_arns = [
+#     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   ]
 
-  tags = {
-    IAC = "True"
-  }
+#   tags = {
+#     IAC = "True"
+#   }
 
-}
+# }
 
-resource "aws_iam_role" "ecr_role" {
-  name = "ecr_role"
+# resource "aws_iam_role" "ecr_role" {
+#   name = "ecr_role"
 
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : "sts:AssumeRoleWithWebIdentity",
-        "Principal" : {
-          "Federated" : "arn:aws:iam::686255959518:oidc-provider/token.actions.githubusercontent.com"
-        },
-        "Condition" : {
-          "StringEquals" : {
-            "token.actions.githubusercontent.com:aud" : [
-              "sts.amazonaws.com"
-            ],
-            "token.actions.githubusercontent.com:sub" : [
-              "repo:ThiagoBarbosa05/rocketseat.ci.api:ref:refs/heads/main"
-            ]
-          }
-        }
-      }
-    ]
-  })
+#   assume_role_policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Effect" : "Allow",
+#         "Action" : "sts:AssumeRoleWithWebIdentity",
+#         "Principal" : {
+#           "Federated" : "arn:aws:iam::686255959518:oidc-provider/token.actions.githubusercontent.com"
+#         },
+#         "Condition" : {
+#           "StringEquals" : {
+#             "token.actions.githubusercontent.com:aud" : [
+#               "sts.amazonaws.com"
+#             ],
+#             "token.actions.githubusercontent.com:sub" : [
+#               "repo:ThiagoBarbosa05/rocketseat.ci.api:ref:refs/heads/main"
+#             ]
+#           }
+#         }
+#       }
+#     ]
+#   })
 
-  inline_policy {
-    name = "ecr-app-permissions"
-    policy = jsonencode({
-      Statement = [
-        {
-          Sid      = "Statement1"
-          Action   = "apprunner:*",
-          Effect   = "Allow",
-          Resource = "*"
+#   inline_policy {
+#     name = "ecr-app-permissions"
+#     policy = jsonencode({
+#       Statement = [
+#         {
+#           Sid      = "Statement1"
+#           Action   = "apprunner:*",
+#           Effect   = "Allow",
+#           Resource = "*"
 
-        },
-        {
-          Sid = "Statement2"
-          Action = [
-            "iam:PassRole",
-            "iam:CreateServiceLinkedRole"
-          ],
-          Effect   = "Allow",
-          Resource = "*"
+#         },
+#         {
+#           Sid = "Statement2"
+#           Action = [
+#             "iam:PassRole",
+#             "iam:CreateServiceLinkedRole"
+#           ],
+#           Effect   = "Allow",
+#           Resource = "*"
 
-        },
-        {
-          Sid = "Statement3"
-          Action = [
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:BatchGetImage",
-            "ecr:BatchCheckLayerAvailability",
-            "ecr:PutImage",
-            "ecr:InitiateLayerUpload",
-            "ecr:UploadLayerPart",
-            "ecr:CompleteLayerUpload",
-            "ecr:GetAuthorizationToken",
-          ]
-          Effect   = "Allow"
-          Resource = "*"
-        }
-      ]
-    })
-  }
+#         },
+#         {
+#           Sid = "Statement3"
+#           Action = [
+#             "ecr:GetDownloadUrlForLayer",
+#             "ecr:BatchGetImage",
+#             "ecr:BatchCheckLayerAvailability",
+#             "ecr:PutImage",
+#             "ecr:InitiateLayerUpload",
+#             "ecr:UploadLayerPart",
+#             "ecr:CompleteLayerUpload",
+#             "ecr:GetAuthorizationToken",
+#           ]
+#           Effect   = "Allow"
+#           Resource = "*"
+#         }
+#       ]
+#     })
+#   }
 
-  tags = {
-    IAC = "True"
-  }
-}
+#   tags = {
+#     IAC = "True"
+#   }
+# }
